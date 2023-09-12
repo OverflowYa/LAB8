4ab334824901d18181c37667dc62eeb580278cab
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-09-12 17:25:50 +0300

    Add files via upload

---
+++ WpfApp4/MyUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+    
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+    
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Heloo!", "Элемент UserControl");
+        }
+    }
+}
