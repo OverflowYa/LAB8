eda1430b7ca2c64f8e59470bf5a5120e5b706d52
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-09-12 15:51:52 +0300

    Add files via upload

---
+++ Labzadanie1/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+    
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Подписались на событие, что запущен объект Application 
+            this.Startup += new StartupEventHandler(App_Startup);
+            // На всякий случай подписались на событие необработанных исключений
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+    
+        void App_DispatcherUnhandledException(object sender, 
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;// Продолжить выполнение
+        }
+    
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            // Создаем объект стартового окна
+            Window1 win = new Window1();
+            // Настраиваем объект окна
+            win.Title = "Перекрывающий заголовок окна";
+            // Показываем окно
+            win.Show();
+        }
+    }
+}
---
+++ Labzadanie1/Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+    
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        System.Windows.Media.Brush color;
+        bool colorFlag = true;
+    
+        public Window1()
+        {
+            //InitializeComponent();
+            Application.LoadComponent(this, new Uri("Window1.xaml", UriKind.Relative));
+    
+            // Регистрируем обработчик события щелчка на кнопке Btn1
+            Btn1.Click += new RoutedEventHandler(Btn1_Click);
+            // Запоминаем начальный цвет фона окна
+            color = this.Window.Background;
+        }
+    
+        void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            // Меняем цвет фона кнопки 
+            if (colorFlag)
+                this.Window.Background = System.Windows.Media.Brushes.White;
+            else
+                this.Window.Background = color;
+    
+            colorFlag = !colorFlag;
+        }
+    }
+}
